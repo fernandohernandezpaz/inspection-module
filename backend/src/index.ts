6d944e0e15f8e40e80b4dd58b6e5ff8d054975b2
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2022-01-29 13:07:08 -0600

    feat: agregando react con typescript dockerizado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Hello worldd!');
 });
 
 app.listen(PORT, () => {
---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+1. docker-compose up
+2. sudo npm i
+3. go to the browser and put http://localhost:3000/
